57bbcde2c168a4dad004e73223cf3a0dcc983fee
jacksonhmg <jacksonmowattgok@gmail.com>
2023-11-04 22:10:13 -0700

    Initial Commit

---
+++ test-shit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test-shit
+//
+//  Created by jackson mowatt gok on 4/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test-shit/test_shitApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_shitApp.swift
+//  test-shit
+//
+//  Created by jackson mowatt gok on 4/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct test_shitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
