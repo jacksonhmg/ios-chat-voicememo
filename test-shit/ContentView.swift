65849553f606b13b76eda833db9fcb9eb073be98
jacksonhmg <jacksonmowattgok@gmail.com>
2023-11-06 13:30:26 -0800

    playing and pausing local audio

--- test-shit/ContentView.swift
+++ test-shit/ContentView.swift
@@ -6,17 +6,46 @@
 //
 
 import SwiftUI
+import AVKit
+
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State var audioPlayer: AVAudioPlayer!
+        var body: some View {
+                VStack {
+                        Text("play").font(.system(size: 45)).font(.largeTitle)
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            self.audioPlayer.play()
+                        }) {
+                            Image(systemName: "play.circle.fill").resizable()
+                                .frame(width: 50, height: 50)
+                                .aspectRatio(contentMode: .fit)
+                        }
+                        Spacer()
+                        Button(action: {
+                            self.audioPlayer.pause()
+                        }) {
+                            Image(systemName: "pause.circle.fill").resizable()
+                                .frame(width: 50, height: 50)
+                                .aspectRatio(contentMode: .fit)
+                        }
+                        Spacer()
+                    }
+                }
+                .onAppear {
+                    if let sound = Bundle.main.path(forResource: "audio", ofType: "m4a") {
+                        do {
+                            self.audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound))
+                        } catch {
+                            print("Failed to initialize the audio player: \(error)")
+                        }
+                    } else {
+                        print("Audio resource not found.")
+                    }
+                }
         }
-        .padding()
-    }
 }
 
 #Preview {
